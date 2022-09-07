6d8e917f4197a59592a1f30188d0c40d297c5779
timothy@bttrcode <timothy@bttrcode.nl>
2022-09-07 22:10:24 +0200

    Add basic instructions

--- src/components/JsonInput.js
+++ src/components/JsonInput.js
@@ -3,6 +3,10 @@ const JsonInput = ({input, setInput, formatInput, valid}) => {
 
     return (
         <div className="json-input">
+            <p className="json-input__instructions">
+                {"Please paste your JSON object here, and hit the \"Format\" button to get the output below. "}
+                {"The top level object cannot be an array, but arrays nested inside are supported."}
+            </p>
             <textarea className="json-input__input" value={input} onChange={e => setInput(e.target.value)} ></textarea>
             {!valid && <p className="json-input__error">{"Input is not a valid object"}</p>}
             <button
